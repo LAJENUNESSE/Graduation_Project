5627b49efee98c871bab826cc201326036634259
LAJENUNESSE <739854689@qq.com>
2026-03-30 21:45:34 +0800

    [vulkan] resource: 添加 VulkanFramebuffer (RenderPass + Attachments)

--- Engine/src/Renderer/Framebuffer.cpp
+++ Engine/src/Renderer/Framebuffer.cpp
@@ -5,6 +5,10 @@
 #include "Platform/OpenGL/OpenGLFramebuffer.h"
 #include "Renderer/RendererAPI.h"
 
+#ifdef ENGINE_ENABLE_VULKAN
+#include "Platform/Vulkan/VulkanFramebuffer.h"
+#endif
+
 namespace Engine
 {
 
@@ -18,8 +22,12 @@ namespace Engine
         case RendererAPI::API::OpenGL:
             return CreateRef<OpenGLFramebuffer>(spec);
         case RendererAPI::API::Vulkan:
-            ENGINE_CORE_RELEASE_ASSERT(false, "RendererAPI::Vulkan is not yet implemented!");
+#ifdef ENGINE_ENABLE_VULKAN
+            return CreateRef<VulkanFramebuffer>(spec);
+#else
+            ENGINE_CORE_RELEASE_ASSERT(false, "Vulkan support not compiled in!");
             return nullptr;
+#endif
         }
 
         ENGINE_CORE_RELEASE_ASSERT(false, "Unknown RendererAPI!");
