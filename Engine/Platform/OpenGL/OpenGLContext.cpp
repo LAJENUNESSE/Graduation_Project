00e8347d0088c17928f785107eb9ce8d1ffe0e96
LAJENUNESSE <739854689@qq.com>
2026-04-20 18:37:39 +0800

    refactor: RendererCapabilities 查询从 OpenGLContext 移到 Renderer::Init()

--- Engine/Platform/OpenGL/OpenGLContext.cpp
+++ Engine/Platform/OpenGL/OpenGLContext.cpp
@@ -8,7 +8,6 @@
 
 #include "Core/Assert.h"
 #include "Core/Log.h"
-#include "Renderer/RendererCapabilities.h"
 
 namespace Engine
 {
@@ -28,8 +27,6 @@ namespace Engine
         ENGINE_CORE_INFO("  Vendor:   {0}", reinterpret_cast<const char*>(glGetString(GL_VENDOR)));
         ENGINE_CORE_INFO("  Renderer: {0}", reinterpret_cast<const char*>(glGetString(GL_RENDERER)));
         ENGINE_CORE_INFO("  Version:  {0}", reinterpret_cast<const char*>(glGetString(GL_VERSION)));
-
-        RendererCapabilities::Get().Query();
     }
 
     void OpenGLContext::SwapBuffers()
